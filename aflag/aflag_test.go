86d14ea433e6f666746744fdcc87cba9a711d300
mlctrez <mlctrez@gmail.com>
2018-06-19 22:34:07 -0500

    formatting

--- aflag/aflag_test.go
+++ aflag/aflag_test.go
@@ -8,11 +8,11 @@ func TestAccessFlag_String(t *testing.T) {
 	if "public" != MethodAccessFlags(mi[0].Bits).String() {
 		t.Error("access flags public test fail")
 	}
-	if "public static" != MethodAccessFlags(mi[0].Bits|mi[3].Bits).String() {
+	if "public static" != MethodAccessFlags(mi[0].Bits | mi[3].Bits).String() {
 		t.Error("access flags 'public static' test fail")
 	}
 
-	if "public static final" != MethodAccessFlags(mi[0].Bits|mi[3].Bits|mi[4].Bits).String() {
+	if "public static final" != MethodAccessFlags(mi[0].Bits | mi[3].Bits | mi[4].Bits).String() {
 		t.Error("access flags 'public static final' test fail")
 	}
 
